58b477efba70be9cccd39867d4a9e31b18fb3631
Kaspar Schiess <eule@space.ch>
2009-08-30 13:38:18 +0200

    . refactored and commented

--- lib/thrift_amqp/server_transport.rb
+++ lib/thrift_amqp/server_transport.rb
@@ -2,23 +2,47 @@
 
 # Server transport for using oneway thrift over an AMQP-queue. 
 #
-class Thrift::AMQP::ServerTransport
+# Example: 
+#
+#   class MyAwesomeHandler
+#     def battleCry(message)
+#       puts messages
+#     end
+#   end
+# 
+#   handler = MyAwesomeHandler.new()
+#   processor = AwesomeService::Processor.new(handler)
+# 
+#   # Connecting to the exchange called 'battle_cry'. This must match the
+#   # clients setting.    
+#   server_transport = Thrift::AMQP::ServerTransport.new('battle_cry')
+# 
+#   server = Thrift::SimpleServer.new(processor, transport)
+# 
+#   server.serve    # never returns
+#
+class Thrift::AMQP::ServerTransport < Thrift::BaseServerTransport
+  # Initializes a connection to the AMQP queue. If you provide a headers 
+  # argument, only messages that match ALL headers will be accepted. 
+  #
+  # Example: 
+  #
+  #   Thrift::AMQP::ServerTransport.new('queue', :version => 1)
+  #   # Will only match messages that have 'version' == '1'
+  # 
   def initialize(exchange_name, headers={})
     @exchange_name = exchange_name
     @header_filter = transform_hash_to_string(headers)
   end
   
+  # Part of the server 
   def listen
     @transport = Thrift::AMQP::Transport.connect(@exchange_name)
   end
 
   def accept
     @transport
-  end  
-  
-  def close
   end
-
 private
   def transform_hash_to_string(hash)
     hash.inject({}) do |new_hash, (k,v)|
