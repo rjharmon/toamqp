2ce0af11b296bcd06b7af598fee78524b6707d34
Kaspar Schiess <eule@space.ch>
2009-09-10 13:44:49 +0200

    . server transport spec cleaned

--- spec/unit/server_transport_spec.rb
+++ spec/unit/server_transport_spec.rb
@@ -1,48 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Thrift::AMQP::ServerTransport do
-  it "should be constructed with an exchange name" do
-    Thrift::AMQP::ServerTransport.new('exchange')
-  end 
-  it "should be constructed with an exchange and a set of headers" do
-    Thrift::AMQP::ServerTransport.new('exchange', :foo => :bar)
-  end 
-  
   context '#new(exchange)' do
     attr_reader :transport
-    attr_reader :bunny, :queue, :exchange
+    attr_reader :queue, :exchange
 
     before(:each) do
-      @transport = Thrift::AMQP::ServerTransport.new('exchange')
-
-      # Stub Bunny subsystem
-      @bunny = flexmock(:bunny)
-      @exchange = flexmock(:exchange, :name => 'exchange')
-      @queue = flexmock(:queue)
-      
-      set_defaults(bunny, 
-        :start => nil, 
-        :exchange => exchange, 
-        :queue => queue)
-
-      set_defaults(queue, 
-        :bind => nil)
-
-      flexmock(Bunny, :new => bunny)
+      @exchange = flexmock(:exchange)
+      @queue    = flexmock(:queue)
+      @transport = Thrift::AMQP::ServerTransport.new(flexmock(:exchange), flexmock(:queue))
     end
 
-    describe "#listen (1: setup the connection)" do
-      it "should create a bunny connection" do
-        bunny.should_receive(:start).once
-        
-        transport.listen
-      end 
-      it "should create an exchange of type headers" do
-        bunny.should_receive(:exchange).with('exchange', Hash).once
-        
-        transport.listen
-      end 
-    end
     describe "#accept (2: accept a connection, returns a transport)" do
       before(:each) do
         transport.listen
