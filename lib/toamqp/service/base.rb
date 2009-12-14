c17ec5b56b97e9fc351d79daceda8ba5739fe9e6
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2009-12-14 16:52:35 +0100

    + # exchange method on Base

--- lib/toamqp/service/base.rb
+++ lib/toamqp/service/base.rb
@@ -1,2 +1,7 @@
 class TOAMQP::Service::Base
+  
+  class << self # CLASS METHODS
+    def exchange(name)
+    end
+  end
 end
--- spec/unit/service_spec.rb
+++ spec/unit/service_spec.rb
@@ -2,5 +2,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe TOAMQP::Service::Base do
-  
+  context "a deriving class" do
+    attr_reader :klass
+    before(:each) do
+      @klass = SpecService = Class.new(TOAMQP::Service::Base)
+    end
+    it "should define an exchange to use" do
+      klass.instance_eval { exchange :test }
+    end
+  end
 end
