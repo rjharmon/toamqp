fb546c026913674d1b21a00e18e8a25a46dd1bb7
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2009-12-15 18:38:06 +0100

    . design docu

--- lib/toamqp/bridge.rb
+++ lib/toamqp/bridge.rb
@@ -13,6 +13,21 @@ class TOAMQP::Bridge
   # to the AMQP server. 
   #
   def protocol
-    :bridge_generated_protocol
+    Thrift::BinaryProtocol.new(transport)
+  end
+  
+  # Creates and returns a Thrift transport instance that will transport its
+  # messages via AMQP to the exchange. 
+  #
+  def transport
+    # client_source = Source::Queue.new(private_queue_name)
+    # filtered_source = Source::BoundQueue.new(exchange_name, filter_queue_name(:filter => :yes))
+    # broadcast_source = Source::BoundQueue.new(exchange_name, private_queue_name)
+    # private_server_source = Source::Queue.new(private_queue_name)
+    # 
+    # client_dest = Target::Exchange.new(exchange_name)
+    # client_dest2 = Target::Queue.new(private_queue_name)
+    # 
+    # server_dest = Target::Queue.new(reply_queue_name)
   end
 end
--- spec/unit/bridge_spec.rb
+++ spec/unit/bridge_spec.rb
@@ -1,5 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+require 'toamqp/client'
+
 describe TOAMQP::Bridge do
+  attr_reader :connection
+  before(:each) do
+    @connection = flexmock(:connection)
+  end
   
+  def bridge
+    TOAMQP::Bridge.new(connection, 'exchange_name')
+  end
+  
+  describe "#protocol" do
+    def call
+      bridge.protocol
+    end
+    
+    it "should return an instance of Thrift::BinaryProtocol" do
+      call.should be_an_instance_of(Thrift::BinaryProtocol)
+    end 
+  end
 end
