14b4b6e385f3eeb8bc5d6810d945ce5230df2c57
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2009-12-23 14:29:32 +0100

    + Profiling enabled, spec back in

--- spec/integration/test_service_spec.rb
+++ spec/integration/test_service_spec.rb
@@ -59,13 +59,13 @@ describe "Server of test service" do
       # Since we didn't wait for the server, it didn't do the work.
       received_messages.should be_empty
     end
-    xit "should transmit hundreds of messages" do
-      200.times do |i|
+    it "should transmit hundreds of messages" do
+      100.times do |i|
         client.announce("message #{i}")
       end
       server.serve
       
-      received_messages.should have(200).messages
+      received_messages.should have(100).messages
     end 
     context "server" do
       it "should receive the call" do
